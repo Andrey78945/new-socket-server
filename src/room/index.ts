bac3db6e8d76209f7d9b02cf8584ca477fef03e7
Andrei <platonovaudit@yandex.ru>
2023-02-13 16:09:55 +0500

    feat: add common videochat

--- src/room/index.ts
+++ src/room/index.ts
@@ -1,16 +1,46 @@
 import { Socket } from "socket.io";
 import { v4 as uuidV4 } from "uuid";
 
-export const roomHandler = (socket: Socket) => {
-  const joinRoom = ({ roomId }: { roomId: string }) => {
-    console.log("user join the room", roomId);
-  };
+const rooms: Record<string, string[]> = {};
 
+interface RoomParams {
+  roomId: string;
+  peerId: string;
+}
+
+export const roomHandler = (socket: Socket) => {
   const createRoom = () => {
     const roomId = uuidV4();
-    socket.join(roomId);
+    rooms[roomId] = [];
+
     socket.emit("room-created", { roomId });
     console.log("the room was created");
+  };
+
+  const joinRoom = ({ roomId, peerId }: RoomParams) => {
+    if (rooms[roomId]) {
+      console.log(`user: ${peerId} join the room:  ${roomId}`);
+
+      socket.join(roomId);
+      if (!rooms[roomId].includes(peerId)) rooms[roomId].push(peerId);
+
+      socket.to(roomId).emit("user-joined", { peerId });
+
+      socket.emit("get-users", {
+        roomId,
+        members: rooms[roomId],
+      });
+    }
+
+    socket.on("disconnect", () => {
+      console.log(`user: ${peerId} left the room`);
+      leaveRoom({ roomId, peerId });
+    });
+  };
+
+  const leaveRoom = ({ roomId, peerId }: RoomParams) => {
+    rooms[roomId] = rooms[roomId].filter((id: string) => id !== peerId);
+    socket.to(roomId).emit("user-disconnected", peerId);
   };
 
   socket.on("create-room", createRoom);
